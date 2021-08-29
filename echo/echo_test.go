74734015c8064c95c8988e69056c199e162d1dfb
Dmitrii Limonov <disordervlz@gmail.com>
2021-08-29 21:33:23 +0300

    copy p2p example testutils add echo example

---
+++ echo/echo_test.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"context"
+	"log"
+	"p2p_test/testutils"
+	"testing"
+)
+
+func TestMain(t *testing.T) {
+	var h testutils.LogHarness
+	h.Expect("listening for connections")
+	h.Expect("sender opening stream")
+	h.Expect("sender saying hello")
+	h.Expect("listener received new stream")
+	h.Expect("read: Hello, world!")
+	h.Expect(`read reply: "Hello, world!\n"`)
+
+	h.Run(t, func() {
+		// Create a context that will stop the hosts when the tests end
+		ctx, cancel := context.WithCancel(context.Background())
+		defer cancel()
+
+		// Get a tcp port for the listener
+		lport, err := testutils.FindFreePort(t, "", 5)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		// Get a tcp port for the sender
+		sport, err := testutils.FindFreePort(t, "", 5)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		// Make listener
+		lh, err := makeBasicHost(lport, true, 1)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		startListener(ctx, lh, lport, true)
+
+		// Make sender
+		listenAddr := getHostAddress(lh)
+		sh, err := makeBasicHost(sport, true, 2)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		runSender(ctx, sh, listenAddr)
+	})
+}
--- go.mod
+++ go.mod
@@ -6,6 +6,8 @@ require github.com/libp2p/go-libp2p v0.14.4
 
 require (
 	github.com/gogo/protobuf v1.3.2
+	github.com/golang/protobuf v1.5.2
+	github.com/ipfs/go-log/v2 v2.1.3
 	github.com/libp2p/go-libp2p-core v0.8.6
 	github.com/libp2p/go-libp2p-kad-dht v0.13.1
 	github.com/libp2p/go-libp2p-mplex v0.4.1
@@ -15,4 +17,5 @@ require (
 	github.com/libp2p/go-tcp-transport v0.2.7
 	github.com/libp2p/go-ws-transport v0.4.0
 	github.com/multiformats/go-multiaddr v0.3.3
+	google.golang.org/protobuf v1.26.0
 )
